4414032345ade81dd96c9ec86976757633a16e3f
rakesh-droid <rakeshleonel@gmail.com>
2020-08-22 19:39:47 +0530

    updated all the files

--- GYM Model.py
+++ GYM Model.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan  8 22:01:24 2019
 
-@author: kanth
-"""
 
 import pandas as pd
 import numpy as np
@@ -12,53 +6,50 @@ from sklearn import datasets
 from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-
-
-data = pd.read_excel('dataGYM.xlsx')
-
-data.columns
-
-del data['BMI']
-
-
-
-
-X = data.iloc[:,:3]
-
-y = data.iloc[:,3:]
-
-
 from sklearn.preprocessing import LabelEncoder
 
+data = pd.read_excel(r'C:\Users\rakes\Downloads\dataGYM.xlsx')
+
+df = data.copy()
+del df['BMI']
+del df['Class']
 label_encoder = LabelEncoder()
-data['Prediction'] = label_encoder.fit_transform(data['Prediction'])
+df['Prediction'] = label_encoder.fit_transform(df['Prediction'])
+
+
+X = df.iloc[:,:-1]
+y = df.iloc[:,-1]
 
 
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=0)
 
-model_GYM = RandomForestClassifier()
+model_GYM = RandomForestClassifier(n_estimators=20)
 
-
-
-model_GYM = LogisticRegression()
 model_GYM.fit(X_train, y_train)
 
 print(model_GYM)
+
+
 # make predictions
 expected = y_test
 predicted = model_GYM.predict(X_test)
 # summarize the fit of the model
 #Correction
-print(metrics.classification_report(expected, predicted))
-print(metrics.confusion_matrix(expected, predicted))
+metrics.classification_report(expected, predicted)
+metrics.confusion_matrix(expected, predicted)
 
 import pickle
 
-pickle.dump(model, open("model_GYM.pkl", "wb" ))
+pickle.dump(model_GYM, open("model_GYM.pkl", "wb" ))
 
-my_scaler = pickle.load(open("model_GYM.pkl", "rb" ))
+model = pickle.load(open("model_GYM.pkl", "rb" ))
+
+print(model.predict([[40,5.6,70]]))
+
+
+
 
 
 
